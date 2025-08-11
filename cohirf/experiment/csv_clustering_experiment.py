8381f6c7443218f7ffab80ba3220828e3658335a
Bruno Belucci <bruno.belucci-teixeira@dauphine.eu>
2025-08-11 16:57:58 +0200

    update csv experiment

--- cohirf/experiment/csv_clustering_experiment.py
+++ cohirf/experiment/csv_clustering_experiment.py
@@ -1,11 +1,9 @@
-import argparse
-from itertools import product
 from typing import Optional
 import mlflow
-import numpy as np
 import pandas as pd
-
+from cohirf.experiment.open_ml_clustering_experiment import preprocess
 from cohirf.experiment.clustering_experiment import ClusteringExperiment
+from pathlib import Path
 
 
 class CSVClusteringExperiment(ClusteringExperiment):
@@ -18,10 +16,11 @@ class CSVClusteringExperiment(ClusteringExperiment):
     continuous features, and removing zero-variance features. The experiment is designed to work
     with diverse real-world datasets that require preprocessing before clustering.
     """
-    
+
     def __init__(
             self,
-            datasets_names: Optional[list[str]] = None,
+            dataset_name: Optional[str | list[str]] = None,
+            standardize: bool = False,
             **kwargs
     ):
         """
@@ -35,25 +34,31 @@ class CSVClusteringExperiment(ClusteringExperiment):
             **kwargs: Additional keyword arguments passed to parent class.
         """
         super().__init__(**kwargs)
-        self.datasets_names = datasets_names
+        self.dataset_name = dataset_name
+        self.standardize = standardize
 
     def _add_arguments_to_parser(self):
         super()._add_arguments_to_parser()
-        self.parser.add_argument('--datasets_names', type=str, nargs='*')
+        self.parser.add_argument("--dataset_name", type=str, nargs="*")
+        self.parser.add_argument('--standardize', action='store_true')
 
     def _unpack_parser(self):
         args = super()._unpack_parser()
-        self.datasets_names = args.datasets_names
+        self.dataset_name = args.dataset_name
+        self.standardize = args.standardize
         return args
 
-    def _load_data(self, combination: dict, unique_params: dict, extra_params: dict, **kwargs):
-        csv_data = pd.read_csv('csv_data.csv', sep=';',
+    def _load_data(
+        self, combination: dict, unique_params: dict, extra_params: dict, mlflow_run_id: Optional[str] = None, **kwargs
+    ):
+        csv_data = pd.read_csv(Path(__file__).parent / 'csv_data.csv', sep=';',
                                dtype={'dataset_name': str, 'X_path': str, 'y_path': str, 'cat_features': str})
         dataset_name = combination['dataset_name']
         dataset_row = csv_data[csv_data['dataset_name'] == dataset_name]
         X_path = dataset_row['X_path'].values[0]
         y_path = dataset_row['y_path'].values[0]
         cat_features = dataset_row['cat_features'].values[0]
+        standardize = unique_params["standardize"]
         # transform nan to 'None'
         if pd.isna(cat_features):
             cat_features = 'None'
@@ -69,35 +74,7 @@ class CSVClusteringExperiment(ClusteringExperiment):
         cont_features_names = [feature for feature in X.columns if feature not in cat_features_names]
         n_classes = len(y.unique())
         # we will preprocess the data always in the same way
-        # categorical features
-        if cat_features_names:
-            # we will convert categorical features to codes
-            for cat_feature in cat_features_names:
-                X[cat_feature] = X[cat_feature].astype('category').cat.codes
-                X[cat_feature] = X[cat_feature].replace(-1, np.nan).astype('category')
-            # we will fill missing values with the most frequent value
-            X[cat_features_names] = X[cat_features_names].fillna(X[cat_features_names].mode().iloc[0])
-            # we will one hot encode the categorical features and convert them to float
-            # but only if they have less than 10 categories, else we drop them
-            cat_dims = [len(X[cat_feature].cat.categories) for cat_feature in cat_features_names]
-            cat_features_names_more_10 = [cat_feature for cat_feature, cat_dim in zip(cat_features_names, cat_dims) if
-                                          cat_dim < 10]
-            X = pd.get_dummies(X, columns=cat_features_names_more_10, drop_first=True)
-            cat_features_drop = [cat_feature for cat_feature in cat_features_names if
-                                 cat_feature not in cat_features_names_more_10]
-            X = X.drop(columns=cat_features_drop)
-        # continuous features
-        if cont_features_names:
-            # we will fill missing values with the median
-            X[cont_features_names] = X[cont_features_names].fillna(X[cont_features_names].median())
-            # we will standardize the continuous features
-            X[cont_features_names] = (X[cont_features_names] - X[cont_features_names].mean()) / X[
-                cont_features_names].std()
-            # we will cast them to float
-            X[cont_features_names] = X[cont_features_names].astype(float)
-        # we will drop 0 variance features
-        X = X.dropna(axis=1, how='all')
-
+        X, y = preprocess(X, y, cat_features_names, cont_features_names, standardize)
         # log to mlflow to facilitate analysis
         mlflow_run_id = extra_params.get('mlflow_run_id', None)
         if mlflow_run_id is not None:
@@ -114,50 +91,21 @@ class CSVClusteringExperiment(ClusteringExperiment):
             'dataset_name': dataset_name
         }
 
-    def _get_combinations(self):
-        if self.datasets_names is None:
-            raise ValueError('datasets_names must be specified')
-        combinations = list(product(self.models_nickname, self.seeds_models, self.datasets_names))
-        combination_names = ['model_nickname', 'seed_model', 'dataset_name']
-        combinations = [list(combination) + [self.models_params[combination[0]]] + [self.fits_params[combination[0]]]
-                        for combination in combinations]
-        combination_names += ['model_params', 'fit_params']
-        unique_params = dict()
-        extra_params = dict(n_jobs=self.n_jobs, return_results=False, timeout_combination=self.timeout_combination,
-                            timeout_fit=self.timeout_fit)
-        return combinations, combination_names, unique_params, extra_params
+    def _get_combinations_names(self) -> list[str]:
+        combination_names = super()._get_combinations_names()
+        combination_names.extend(
+            [
+                "dataset_name",
+            ]
+        )
+        return combination_names
 
-    def run_csv_experiment_combination(
-            self, model_nickname: str, dataset_name: str, seed_model: int = 0, model_params: Optional[dict] = None,
-            fit_params: Optional[dict] = None,
-            n_jobs: int = 1, return_results: bool = True,
-            log_to_mlflow: bool = False,
-            timeout_combination: Optional[int] = None, timeout_fit: Optional[int] = None,
-    ):
-
-        combination = {
-            'model_nickname': model_nickname,
-            'seed_model': seed_model,
-            'dataset_name': dataset_name,
-            'model_params': model_params,
-            'fit_params': fit_params,
-        }
-        unique_params = {}
-        extra_params = {
-            'n_jobs': n_jobs,
-            'return_results': return_results,
-            'timeout_combination': timeout_combination,
-            'timeout_fit': timeout_fit,
-        }
-        if log_to_mlflow:
-            return self._run_mlflow_and_train_model(combination=combination, unique_params=unique_params,
-                                                    extra_params=extra_params, return_results=return_results)
-        else:
-            return self._train_model(combination=combination, unique_params=unique_params, extra_params=extra_params,
-                                     return_results=return_results)
+    def _get_unique_params(self):
+        unique_params = super()._get_unique_params()
+        unique_params["standardize"] = self.standardize
+        return unique_params
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    experiment = CSVClusteringExperiment(parser=parser)
-    experiment.run()
+    experiment = CSVClusteringExperiment()
+    experiment.run_from_cli()
--- cohirf/experiment/hpo_csv_clustering_experiment.py
+++ cohirf/experiment/hpo_csv_clustering_experiment.py
@@ -1,32 +1,46 @@
 import argparse
 from typing import Optional
-from ml_experiments.hpo_experiment import HPOExperiment
+from cohirf.experiment.hpo_clustering_experiment import HPOClusteringExperiment
 from cohirf.experiment.csv_clustering_experiment import CSVClusteringExperiment
 
 
-class HPOCSVClusteringExperiment(HPOExperiment, CSVClusteringExperiment):
+class HPOCSVClusteringExperiment(HPOClusteringExperiment, CSVClusteringExperiment):
     def get_hyperband_max_resources(self, combination: dict, unique_params: dict, extra_params: dict, **kwargs):
         raise NotImplementedError('Hyperband is not available for this experiment')
 
-    def _load_single_experiment(self, combination: dict, unique_params: dict, extra_params: dict, **kwargs):
-        openml_clustering_experiment = CSVClusteringExperiment(
+    def _load_simple_experiment(
+        self, combination: dict, unique_params: dict, extra_params: dict, mlflow_run_id: str | None = None, **kwargs
+    ):
+        experiment = CSVClusteringExperiment(
             # experiment parameters
-            experiment_name=self.experiment_name, create_validation_set=self.create_validation_set,
-            log_dir=self.log_dir, log_file_name=self.log_file_name, work_root_dir=self.work_root_dir,
-            save_root_dir=self.save_root_dir, clean_work_dir=self.clean_work_dir,
-            raise_on_fit_error=self.raise_on_fit_error, error_score=self.error_score, 
-            mlflow_tracking_uri=self.mlflow_tracking_uri, check_if_exists=self.check_if_exists, verbose=0
+            experiment_name=self.experiment_name,
+            log_dir=self.log_dir,
+            log_file_name=self.log_file_name,
+            work_root_dir=self.work_root_dir,
+            save_root_dir=self.save_root_dir,
+            clean_work_dir=self.clean_work_dir,
+            clean_data_dir=False,
+            raise_on_error=self.raise_on_error,
+            mlflow_tracking_uri=self.mlflow_tracking_uri,
+            check_if_exists=self.check_if_exists,
+            profile_memory=self.profile_memory,
+            profile_time=self.profile_time,
+            verbose=0,
         )
-        return openml_clustering_experiment
+        return experiment
 
-    def _load_data(self, combination: dict, unique_params: dict, extra_params: dict, **kwargs):
+
+def _load_data(
+        self, combination: dict, unique_params: dict, extra_params: dict, mlflow_run_id: Optional[str] = None, **kwargs
+    ):
         dataset_name = combination['dataset_name']
+        standardize = unique_params['standardize']
         return {
-            'dataset_name': dataset_name
+            'dataset_name': dataset_name,
+            'standardize': standardize
         }
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    experiment = HPOCSVClusteringExperiment(parser=parser)
-    experiment.run()
+    experiment = HPOCSVClusteringExperiment()
+    experiment.run_from_cli()
